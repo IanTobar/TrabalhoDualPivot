4b1f54fc164d67592ccdf1089a0b5c1deabfba0f
liviagranato4 <38290456+liviagranato4@users.noreply.github.com>
2018-04-11 23:05:11 -0300

    Create Funcoes.h

---
+++ DualPivot/Funcoes.h
@@ -0,0 +1,8 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+void troca (int* a, int* b);
+void qSDualPivot (int* vet, int menorPos, int maiorPos);
+int divide(int* vet, int menorPos, int maiorPos, int*pivoEsq);
+
+#endif 
