2f69160e7d0c3cc8e3d13147e04cb9183237c879
liviagranato4 <38290456+liviagranato4@users.noreply.github.com>
2018-04-14 23:47:08 -0300

    Update main.c

--- DualPivot/main.c
+++ DualPivot/main.c
@@ -1,24 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: tobar
- *
- * Created on 11 de Abril de 2018, 12:09
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "ES.h"
+#include "Funcoes.h"
 
 /*
  * 
  */
 int main(int argc, char** argv) {
 
+    FILE *a; //Ponteiro que vai apontar para o arquivo
+    FILE *b;
+    int op, tamanho_vetor; //op = variavel para armazenar opções/tamanho_vetor = guarda o tamanho do vetor
+    char entrada[100], saida[100];
+    printf("Digite o nome do arquivo\n");
+    scanf(" %[^\n]s", entrada);
+    
+    //Pega o tamanho do vetor
+    printf("Qual o tamanho do vetor?\n");
+    scanf("%d", &tamanho_vetor);
+
+    //Aloca o vetor dinamicamente por malloc
+    int *v = (int*) malloc(tamanho_vetor * sizeof (int));
+    cria_arquivo(a, entrada);
+    escreveArquivo(a, entrada, tamanho_vetor);
+    lerArquivo(entrada, v);
+
+    //MENU
+    do {
+        printf("[1]Ordenar por DualPivot QuickSort (ordem crescente)\n");
+        printf("[2]Ordenar por DualPivot QuickSort (ordem decrescente)\n");
+        printf("[3]Sair\n");
+        scanf("%d", &op);
+        
+        switch (op) {
+
+            case 1:
+                printf("Digite o nome de arquivo de saida\n");
+                scanf(" %[^\n]s", saida);
+                qSDualPivotCrescente(v, 0, tamanho_vetor);
+                salvaArquivo(b, saida, tamanho_vetor, v);
+               
+                break;
+            case 2:
+                printf("Digite o nome de arquivo de saida\n");
+                scanf(" %[^\n]s", saida);
+                qSDualPivotDecrescente(v, 0, tamanho_vetor);
+                salvaArquivo(b, saida, tamanho_vetor, v);                           
+                break;
+            case 3:
+                exit(1);
+                break;
+            default:
+                break;        
+        }
+    } while (op != 3);
     return (EXIT_SUCCESS);
 }
+
 
