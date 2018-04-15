3a37169d182737b9efb9819e9d1e8a631007b24f
Ian Marcelo Tobar <ianeloi@hotmail.com>
2018-04-15 01:17:30 -0300

    Adicionadas funcoes para Leitura Tempo
    utilizando biblioteca <sys/time.h> para conseguir medir o tempo em mili e microsegundos.

--- DualPivot/main.c
+++ DualPivot/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
 #include "ES.h"
 #include "Funcoes.h"
 
@@ -12,10 +13,13 @@ int main(int argc, char** argv) {
     FILE *a; //Ponteiro que vai apontar para o arquivo
     FILE *b;
     int op, tamanho_vetor; //op = variavel para armazenar opções/tamanho_vetor = guarda o tamanho do vetor
+    double ti = 0, tf = 0, tempo = 0; //Tempo Inicial, Tempo Final e Tempo gasto
+    struct timeval tempo_inicial, tempo_final;
     char entrada[100], saida[100];
+
     printf("Digite o nome do arquivo\n");
     scanf(" %[^\n]s", entrada);
-    
+
     //Pega o tamanho do vetor
     printf("Qual o tamanho do vetor?\n");
     scanf("%d", &tamanho_vetor);
@@ -32,30 +36,35 @@ int main(int argc, char** argv) {
         printf("[2]Ordenar por DualPivot QuickSort (ordem decrescente)\n");
         printf("[3]Sair\n");
         scanf("%d", &op);
-        
+
         switch (op) {
 
             case 1:
                 printf("Digite o nome de arquivo de saida\n");
                 scanf(" %[^\n]s", saida);
+                gettimeofday(&tempo_inicial, NULL);
                 qSDualPivotCrescente(v, 0, tamanho_vetor);
+                gettimeofday(&tempo_final, NULL);
+                ti = (double) tempo_inicial.tv_usec + ((double) tempo_inicial.tv_sec * (1000000.0));
+                tf = (double) tempo_final.tv_usec + ((double ) tempo_final.tv_sec * (1000000.0));
+                tempo = (tf - ti) / 1000;
+                
                 salvaArquivo(b, saida, tamanho_vetor, v);
-               
+
                 break;
             case 2:
                 printf("Digite o nome de arquivo de saida\n");
                 scanf(" %[^\n]s", saida);
                 qSDualPivotDecrescente(v, 0, tamanho_vetor);
-                salvaArquivo(b, saida, tamanho_vetor, v);                           
+                salvaArquivo(b, saida, tamanho_vetor, v);
                 break;
             case 3:
                 exit(1);
                 break;
             default:
-                break;        
+                break;
         }
     } while (op != 3);
     return (EXIT_SUCCESS);
 }
-
 
