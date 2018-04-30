628f4a045ecd3e1517bbf7a476b3e224a043bfe3
Ian Marcelo Tobar <ianeloi@hotmail.com>
2018-04-29 22:34:51 -0300

    Adequando o programa
    Adequando o programa aos requisitos da Vanessa, uso de ARGV, padrão adequado do arquivo de saida, tirada das linhas de interação com o usuário na main, removendo funções que pelos padrões da vanessa não deveriam integra esse programa como criar arquivos randômicos etc...

--- DualPivot/ES.c
+++ DualPivot/ES.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "Funcoes.h"
 
 void lerArquivo(char *nomeArquivo, int *vet) {
     FILE *entrada;
@@ -16,24 +17,26 @@ void lerArquivo(char *nomeArquivo, int *vet) {
 }
 
 //Cria um arquivo
+
 void cria_arquivo(FILE *a, char *entrada) {
-        if ((a = fopen(entrada, "a")) != NULL) {
-            printf("Arquivo criado!\n");
-            fclose(a);
-        } else {
-            printf("Houve um erro ao tentar criar o arquivo!\n");
-            fclose(a);
-            exit(1);
-        }
+    if ((a = fopen(entrada, "a")) != NULL) {
+        printf("Arquivo criado!\n");
+        fclose(a);
+    } else {
+        printf("Houve um erro ao tentar criar o arquivo!\n");
+        fclose(a);
+        exit(1);
+    }
 }
 
 //Printa o vetor na tela
-void printa_vetor(int *v,int tamanho_vetor, FILE*a){
+
+void printa_vetor(int *v, int tamanho_vetor, FILE*a) {
     char op;
     char nome[100];
-    
-    for(int i=0;i<tamanho_vetor;i++){
-        printf("%d\n",v[i]);
+
+    for (int i = 0; i < tamanho_vetor; i++) {
+        printf("%d\n", v[i]);
     }
 
     printf("Digite o nome do arquivo para salvar a ordenacao:\n");
@@ -46,30 +49,37 @@ void printa_vetor(int *v,int tamanho_vetor, FILE*a){
         fclose(a);
         exit(1);
     }
-    
-    for (int j=0; j<tamanho_vetor;j++){
-    fprintf(a, "%d\n", v[j]);
+
+    for (int j = 0; j < tamanho_vetor; j++) {
+        fprintf(a, "%d\n", v[j]);
     }
     fclose(a);
-return;
+    return;
 }
 
-void escreveArquivo(FILE* a, char *entrada, int tamanho_vetor){
+void escreveArquivo(FILE* a, char *entrada, int tamanho_vetor) {
     int i;
-    
+
     a = fopen(entrada, "a+");
-    for (i=0;i<tamanho_vetor+1; i++){
-        if (i==0) fprintf(a, "%d\n", tamanho_vetor);
-        else fprintf(a, "%d\n", rand() % 500000);        
+    for (i = 0; i < tamanho_vetor + 1; i++) {
+        if (i == 0) fprintf(a, "%d\n", tamanho_vetor);
+        else fprintf(a, "%d\n", rand() % 500000);
     }
     fclose(a);
 }
 
-void salvaArquivo(FILE* b, char *saida, int tamanho_vetor, int *v){
+void salvaArquivo(FILE* b, char *saida, int tamanho_vetor, int *v) {
     int i;
-    b=fopen(saida, "a+");
-    for (i=0; i<tamanho_vetor+1; i++){
-        if(i!=0) fprintf(b, "%d\n",v[i]);
+    b = fopen(saida, "a+");
+    for (i = 0; i < tamanho_vetor + 1; i++) {
+        if (i != 0) fprintf(b, "%d\n", v[i]);
     }
     fclose(b);
+}
+
+void salvaArquivoSaida(relatorio *r, double tempo,char *saida) {
+    FILE *f;
+    f = fopen (saida,"w");
+    fprintf (f,"%d;%d;%f",r->numComp,r->numTrocas,tempo);
+
 }
--- DualPivot/main.c
+++ DualPivot/main.c
@@ -4,72 +4,94 @@
 #include <time.h>
 #include "ES.h"
 #include "Funcoes.h"
+#include <sys/time.h>
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
-    FILE *a; //Ponteiro que vai apontar para o arquivo
-    FILE *b;
-    int op, tamanho_vetor; //op = variavel para armazenar opções/tamanho_vetor = guarda o tamanho do vetor
-    char entrada[100], saida[100];
+    //Adicionando entrada por linha de comando
+    //Padrao instituido: argv[0] = Nome do programa
+    //argv[1] = Nome Arquivo de Entrada
+    //argv[2] = Tamanho do Arquivo de entrada (vetor de entrada) 
+    //argv[3] = Nome do Arquivo de Saida
+    //argv[4] = Tipo do quickSort (1 = tradicional),(2 = dualPivot)
+
+
+    FILE *entrada; //Ponteiro que vai apontar para o arquivo de entrada
+    FILE *saida; //Ponteiro que vai apontar para o arquivo de saida
+    struct timeval tempo_inicial, tempo_final; //estrutura da biblioteca sys/time.h que armazena varios dados sobre o tempo
+    int tamanho_vetor, op;
+    double ti = 0, tf = 0, tempo = 0; //Tempo Inicial, Tempo Final e Tempo gasto (depois de calculados)
+    char saida[100];
     relatorio *r = (relatorio*) malloc(sizeof (relatorio));
 
-    srand(time(NULL));
+    //int op, tamanho_vetor; //op = variavel para armazenar opções/tamanho_vetor = guarda o tamanho do vetor
+    //char entrada[100], saida[100];
 
-    printf("Digite o nome do arquivo\n");
-    scanf(" %[^\n]s", entrada);
 
-    //Pega o tamanho do vetor
+    //FILE
+    //srand(time(NULL));
+
+    //printf("Digite o nome do arquivo\n");
+    //scanf(" %[^\n]s", entrada);
+
+
+
+    /*Pega o tamanho do vetor
     printf("Qual o tamanho do vetor?\n");
-    scanf("%d", &tamanho_vetor);
+    scanf("%d", &tamanho_vetor);*/
 
-    //Aloca o vetor dinamicamente por malloc
-    int *v = (int*) malloc(tamanho_vetor * sizeof (int));
+    tamanho_vetor = atoi(argv[2]);
+    int *v = (int*) malloc(tamanho_vetor * sizeof (int)); //Aloca o vetor dinamicamente por malloc
 
-    for (int i = 0; i < tamanho_vetor; i++)
-        v[i] = tamanho_vetor - i;
+    //for (int i = 0; i < tamanho_vetor; i++)
+    //  v[i] = tamanho_vetor - i;
 
     /*cria_arquivo(a, &entrada);
     escreveArquivo(a, &entrada, tamanho_vetor);
     lerArquivo(&entrada, v);
      */
     //MENU
-    do {
+    /*do {
         resetaStructRelatorio(r);
         printf("[1]Ordenar por DualPivot QuickSort (ordem crescente)\n");
         printf("[2]Ordenar por QuickSort Tradicional\n");
         printf("[3]Sair\n");
         scanf("%d", &op);
+     */
 
-        printaVetor(v, tamanho_vetor);
+    //printaVetor(v, tamanho_vetor);
 
-        switch (op) {
 
-            case 1:
-                printf("Digite o nome de arquivo de saida\n");
-                scanf(" %[^\n]s", saida);
-                qSDualPivotCrescente(v, 0, tamanho_vetor, r);
-                printaVetor(v, tamanho_vetor);
-                printaStructRelatorio(r);
-                //salvaArquivo(b, saida, tamanho_vetor, v);
-                break;
-            case 2:
-                printf("Digite o nome de arquivo de saida\n");
-                scanf(" %[^\n]s", saida);
-                quickSort(v, 0, tamanho_vetor, r);
-                printaVetor(v, tamanho_vetor);
-                printaStructRelatorio(r);
-                //salvaArquivo(b, saida, tamanho_vetor, v);                           
-                break;
-            case 3:
-                exit(1);
-                break;
-            default:
-                break;
-        }
-    } while (op != 3);
+    // printf("Digite o nome de arquivo de saida\n");
+    //scanf(" %[^\n]s", saida);
+    saida = argv[3];
+
+
+    op = atoi(argv[4]);
+    if (op == 2) {
+        gettimeofday(&tempo_inicial, NULL); //Funcao preenche a estrutura com os dados do tempo corrente (usa tempo do sistema operacional)
+        qSDualPivotCrescente(v, 0, tamanho_vetor, r); //chamada da funcao de ordenação DualPivot Quick Sort
+        gettimeofday(&tempo_final, NULL); //Funcao preenche a estrutura com os dados do tempo corrente (usa tempo do sistema operacional)
+        ti = (double) tempo_inicial.tv_usec + ((double) tempo_inicial.tv_sec * (1000000.0)); //utiliza o horario corrente da estrutura e converte para um double
+        tf = (double) tempo_final.tv_usec + ((double) tempo_final.tv_sec * (1000000.0)); //utiliza o horario corrente da estrutura e converte para um double
+        tempo = (tf - ti) / 1000; //Faz a diferença entre tempo inicial e final para saber o tempo corrente, ja convertendo em milésimos de segundo
+        //printaVetor(v, tamanho_vetor);
+        //printaStructRelatorio(r);
+        //salvaArquivo(b, saida, tamanho_vetor, v);
+    } else {
+        gettimeofday(&tempo_inicial, NULL); //Funcao preenche a estrutura com os dados do tempo corrente (usa tempo do sistema operacional)
+        quickSort(v, 0, tamanho_vetor, r); //chamada da funcao de ordenação QuickSort
+        gettimeofday(&tempo_final, NULL); //Funcao preenche a estrutura com os dados do tempo corrente (usa tempo do sistema operacional)
+        ti = (double) tempo_inicial.tv_usec + ((double) tempo_inicial.tv_sec * (1000000.0)); //utiliza o horario corrente da estrutura e converte para um double
+        tf = (double) tempo_final.tv_usec + ((double) tempo_final.tv_sec * (1000000.0)); //utiliza o horario corrente da estrutura e converte para um double
+        tempo = (tf - ti) / 1000; //Faz a diferença entre tempo inicial e final para saber o tempo corrente, ja convertendo em milésimos de segundo
+        //printf("Digite o nome de arquivo de saida\n");
+        //scanf(" %[^\n]s", saida);
+        //printaVetor(v, tamanho_vetor);
+        //printaStructRelatorio(r);
+    }
+    //salvaArquivo(b, saida, tamanho_vetor, v);                           
+
     return (EXIT_SUCCESS);
 }
 
